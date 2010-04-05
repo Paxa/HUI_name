1ef79f15917f0ee3fdc50d75bb9493be9ef5b582
Pavel <pavel.evst@gmail.com>
2010-04-05 15:43:40 +0400

    source headers

--- Source/HUI_name.js
+++ Source/HUI_name.js
@@ -1,3 +1,12 @@
+/*
+---
+description: HUI_name class.
+license: MIT-style
+authors: Pavel Evstigneev
+requires: core:1.2.4:*
+provides: HUI_name
+
+*/
 var HUI_name = new Class({
     Implements: Options,
 
